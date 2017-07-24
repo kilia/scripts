f7638f06d9d531e580d2dc8227c01b13b30d3fb4
kilia <wuyu.zju@163.com>
2017-07-24 18:15:02 +0800

    change colors

--- redmine-better-issue.js
+++ redmine-better-issue.js
@@ -11,10 +11,15 @@
 (function() {
     'use strict';
 
+function hashCode(s){
+    return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a;} ,0);
+}
+
 var i;
 var status_list = document.getElementsByClassName("status");
 var priority_list = document.getElementsByClassName("priority");
 var tracker_list = document.getElementsByClassName("tracker");
+var assigned_list = document.getElementsByClassName("assigned_to");
 
 if(status_list.length != priority_list.length || status_list.length != tracker_list.length) {
     console.log('bad length');
@@ -35,19 +40,25 @@ for(i=0;i<tracker_list.length;i++) {
     var st_style = 'color: #999999';
     if(!is_bug || (is_high && is_bug)) {
         if(st.firstElementChild.innerText == '新建')
-            st_style = 'color: red';
+            st_style = 'color: red; font-weight: bold;';
         else if(st.firstElementChild.innerText == '接受')
-            st_style = 'color: orange';
+            st_style = 'color: blue;';
         else if(st.firstElementChild.innerText == '完成' || st.firstElementChild.innerText == '预发布f测试完成')
-            st_style = 'color: green';
+            st_style = 'color: #448844;';
         else
-            st_style = 'color: red';
+            st_style = 'color: red; font-weight: bold;';
+    }
+
+    var at = assigned_list[i];
+    var name_txt = at.firstElementChild.innerText;
+    if(hashCode(name_txt) == 784544) {
+        //st.parentElement.style = 'background-color:#99dd99; color:#f8f8f8;';
+        at.firstElementChild.outerHTML = '<span style="color:red; background-color:#99dd99;">' + name_txt + '</span>';
     }
 
     pr.firstElementChild.style = st_style;
     st.firstElementChild.style = st_style;
     tk.firstElementChild.style = st_style;
 }
-
 
 })();
